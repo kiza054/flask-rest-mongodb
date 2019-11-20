6db58ee1553c3055e25494e4511ea7c39a5394c6
kiza054 <43473531+kiza054@users.noreply.github.com>
2019-11-20 12:13:22 +0000

    v0.0.1
    changed secret_key and mongo_uri

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.secret_key = "secret key"
-app.config["MONGO_URI"] = "mongodb://localhost:27017/roytuts"
+app.secret_key = "thisisasecret"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/apidemo2019"
 mongo = PyMongo(app)
